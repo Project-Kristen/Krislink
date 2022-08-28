a51846249e61b6fd66df6f4979e9b326034cc8e0
NCT-skyouo <56823143+NCT-skyouo@users.noreply.github.com>
2022-08-28 04:54:44 +0000

    [Minor] Formatting interactionCreate.js

--- test/client/advance/events/interactionCreate.js
+++ test/client/advance/events/interactionCreate.js
@@ -1,13 +1,15 @@
 module.exports = {
 	name: 'interactionCreate',
 	execute(client, interaction) {
+        if (!interaction.isCommand()) return;
+        
 		const commandName = interaction.commandName;
         const command = client.commands.get(commandName);
 
         if (command == null) {
-            return interaction.reply(":x: **未知的指令!**")
+            return interaction.reply(":x: **未知的指令!**");
         }
 
-        command.execute(client, interaction)
+        return command.execute(client, interaction);
 	},
 };
